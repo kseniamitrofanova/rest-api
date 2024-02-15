82db86694aeffc808f8bbf4cfd893906ee511203
KMitrofanova <vip.ksundra@mail.ru>
2024-02-15 12:31:44 +0300

    added test

---
+++ src/test/java/StatusTests.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class StatusTests
+{
+
+    @Test
+    void checkTotal20()
+    {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total",is(20));
+    }
+
+    @Test
+    void checkTotal20WithLogs()
+    {
+        given()
+                .log().all()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total",is(21));
+    }
+
+    @Test
+    void checkTotal20ResponseLogs()
+    {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total",is(21));
+    }
+}
